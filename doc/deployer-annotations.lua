7a1a23fde6761b5b489becc7489f4f37861fb0ab
Tony Lagnada <deloyz@gmail.com>
2026-04-17 12:01:01 -0700

    save point

---
+++ doc/deployer-annotations.lua
@@ -0,0 +1,26 @@
+--- @alias FilePath string
+--
+--
+
+--- @class ProjectAddOnInfo
+--- @field name string?       @The addon name (a folder in the project)
+--- @field as string?         @Deploy addon as this name
+--- @field deploy boolean?
+--
+--
+
+--- @class DeploymentTarget
+--- @field name string?       @The deployment name (usually the wow common name)
+--- @field deploy boolean?
+--- @field dir string
+--
+--
+
+--- @class DeploymentConfig
+--- @field name string
+--- @field version string
+--- @field projectDir FilePath
+--- @field deployments table<string, DeploymentTarget>
+--- @field addons table<string, ProjectAddOnInfo>
+--
+--
